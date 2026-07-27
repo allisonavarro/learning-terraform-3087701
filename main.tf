c7ef63527dabf1af880f53012e08475aa93d07d7
Alisson Navarro <alync316@hotmail.com>
2026-07-27 09:50:54 -0600

    simple example

--- main.tf
+++ main.tf
@@ -1,24 +1,36 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+terraform {
+  cloud {
+    organization = "allisonavarro"
+    workspaces {
+      name = "learning-terraform"
+    }
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+provider "aws" {
+  region = "us-east-1"
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
+resource "aws_s3_bucket" "hello_bucket" {
+  bucket = "hello-world-${random_id.suffix.hex}"
+}
+
+resource "aws_s3_bucket_object" "hello_file" {
+  bucket = aws_s3_bucket.hello_bucket.id
+  key    = "hello.txt"
+  content = "Hello World from Terraform Cloud + AWS!"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.hello_bucket.bucket
 }
