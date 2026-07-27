134a6a84573f8ec19882b2a6e901805e3ea97530
Alisson Navarro <alync316@hotmail.com>
2026-07-27 10:47:57 -0600

    generic ami

--- main.tf
+++ main.tf
@@ -1,21 +1,15 @@
-data "aws_ami" "app_ami" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.nano"
 
   tags = {
