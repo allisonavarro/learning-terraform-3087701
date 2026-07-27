39a51c929d11c7c0bfa3e692e89e2ad28f80ea88
Alisson Navarro <alync316@hotmail.com>
2026-07-27 11:52:19 -0600

    update instance type for free tier

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
