ace9e2cead09024019360b008efd39f3f307299d
Alisson Navarro <alync316@hotmail.com>
2026-07-31 15:32:07 -0600

    use sg module

--- main.tf
+++ main.tf
@@ -14,51 +14,14 @@ data "aws_ami" "app_ami" {
   }
 }
 
-# 2. Create the Security Group for Web Access
-resource "aws_security_group" "tomcat_sg" {
-  name        = "tomcat-web-access"
-  description = "Allow inbound traffic for Tomcat server"
 
-  # HTTP Traffic (Standard Web)
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Tomcat Specific Port
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Change to your IP (e.g., "1.2.3.4/32") for better security
-  }
-
-  # SSH Traffic (Optional, for server management)
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Highly recommended to change this to your specific public IP
-  }
-
-  # Outbound Rules (Allows instance to download internet updates/packages)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# 3. Deploy Free-Tier EC2 Instance
+# Deploy Free-Tier EC2 Instance
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.micro" # Free Tier eligible
 
   # Attach the Security Group
-  vpc_security_group_ids = [aws_security_group.tomcat_sg.id]
+  vpc_security_group_ids = [module.http_80_security_group.id]
 
   # Automatically install Tomcat 10 on boot
   user_data = <<-EOF
@@ -72,6 +35,20 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Tomcat-Free-Tier"
   }
+}
+
+module "http_80_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "~> 6.0"
+
+  name        = "http-80"
+  description = "Security group for http-80"
+  vpc_id  = data.aws_vpc.default.id
+
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 # 4. Output the public URL to easily click and test
