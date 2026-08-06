4aac88a4c823d225089e82e60c5c5a8672b75937
Alisson Navarro <alync316@hotmail.com>
2026-08-06 12:44:49 -0600

    testing a new dev vpc

--- main.tf
+++ main.tf
@@ -14,10 +14,23 @@ data "aws_ami" "app_ami" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
 # Deploy Free-Tier EC2 Instance
 resource "aws_instance" "web" {
@@ -29,6 +42,9 @@ resource "aws_instance" "web" {
     module.http_80_security_group.id,
     module.tomcat_sg.id
     ]
+  # Add subnet 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
 
   # Automatically install Tomcat 10 on boot
   user_data = <<-EOF
@@ -50,7 +66,7 @@ module "http_80_security_group" {
 
   name        = "http-80"
   description = "Security group for http-80"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   # Opens HTTP (port 80) to the Internet
   ingress_cidr_ipv4 = {
@@ -72,7 +88,7 @@ module "tomcat_sg" {
   version = "~> 6.0"
 
   name   = "tomcat-sg"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = {
     tomcat = {
